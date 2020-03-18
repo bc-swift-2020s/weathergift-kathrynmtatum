d12241ca6a9ad99d8612e2b350a1867aab323851
Kathryn Tatum <tatumka@bc.edu>
2020-03-18 14:58:25 -0400

    7.1 Setup

---
+++ Weather Gift/LocationFile.swift
@@ -0,0 +1,15 @@
+//
+//  LocationFile.swift
+//  Weather Gift
+//
+//  Created by Kathryn Tatum on 3/10/20.
+//  Copyright © 2020 Kathryn Tatum. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
--- Weather Gift/ViewController.swift
+++ Weather Gift/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
+//CREATION OF ARRAY WITHIN STRUCT
+    var weatherLocations: [WeatherLocation] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+//HOW TO ADD THINGS TO A STRUCT
+        var weatherLocation = WeatherLocation(name: "Chestnut Hill, MA", latitude: 0, longitude: 0)
+        weatherLocations.append(weatherLocation)
+        weatherLocation = WeatherLocation(name: "Herndon, VA", latitude: 0, longitude: 0)
+        weatherLocations.append(weatherLocation)
+        weatherLocation = WeatherLocation(name: "Prague, Czechia", latitude: 0, longitude: 0)
+        weatherLocations.append(weatherLocation)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
-
-
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing {
+            tableView.setEditing(false, animated: true)
+            sender.title = "Edit"
+            addBarButton.isEnabled = true
+        } else {
+            tableView.setEditing(true, animated: true)
+            sender.title = "Done"
+            addBarButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    
 }
 
+//MAKING CONNECTION BETWEEN TBV AND STRUCT
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weatherLocations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = weatherLocations[indexPath.row].name
+        return cell
+    }
+    
+    
+}
