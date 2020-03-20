57c9d97493029b8e867b552e6a59ae64aabe2127
Kathryn Tatum <tatumka@bc.edu>
2020-03-20 11:32:28 -0400

    7.4 Added .gitignore

---
+++ Weather Gift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  Weather Gift
+//
+//  Created by Kathryn Tatum on 3/20/20.
+//  Copyright © 2020 Kathryn Tatum. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyDA_lTPXbXYSM6J5SperI_0xy4j8HVhfBI"
+}
