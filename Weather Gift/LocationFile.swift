cdcc3e1b6e7051070faa7cc3cd50772813eea3f1
Kathryn Tatum <tatumka@bc.edu>
2020-03-23 13:08:09 -0400

    7.10 DarkSky API

--- Weather Gift/LocationFile.swift
+++ Weather Gift/LocationFile.swift
@@ -19,4 +19,34 @@ class WeatherLocation: Codable {
         self.latitude = latitude
         self.longitude = longitude
     }
+    
+    func getData() {
+        let coordinates = "\(latitude),\(longitude)"
+        let urlString = "\(APIurls.darkSkyURL)\(APIkeys.darkSkyKey)/\(coordinates)"
+        print("🛫 We are accessing the URL \(urlString)")
+        
+        //Create URL
+        guard let url = URL(string: urlString) else {
+            print("ERROR: Could not create URL from \(urlString)")
+            return
+        }
+        
+        //Create Session
+        let session = URLSession.shared
+        
+        //Get data with .dataTask method
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+                print("😎 \(json)")
+            } catch {
+                print("🥵 JSON ERROR: \(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
 }
