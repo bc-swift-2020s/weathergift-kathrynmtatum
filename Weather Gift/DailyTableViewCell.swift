47bfcc31bca526b93d727863dcbb73670b1aa900
Kathryn Tatum <tatumka@bc.edu>
2020-03-30 11:42:53 -0400

    7.15 Weekly Weather Predictions

---
+++ Weather Gift/DailyTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  DailyTableViewCell.swift
+//  Weather Gift
+//
+//  Created by Kathryn Tatum on 3/30/20.
+//  Copyright © 2020 Kathryn Tatum. All rights reserved.
+//
+
+import UIKit
+
+class DailyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dailyImageView: UIImageView!
+    @IBOutlet weak var dailyWeekdayLabel: UILabel!
+    @IBOutlet weak var dailyHighLabel: UILabel!
+    @IBOutlet weak var dailySummaryView: UITextView!
+    @IBOutlet weak var dailyLowLabel: UILabel!
+    
+    var dailyWeather: DailyWeather! {
+        didSet {
+            dailyImageView.image = UIImage(named: dailyWeather.dailyIcon)
+            dailyWeekdayLabel.text = dailyWeather.dailyWeekday
+            dailySummaryView.text = dailyWeather.dailySummary
+            dailyHighLabel.text = "\(dailyWeather.dailyHigh)°"
+            dailyLowLabel.text = "\(dailyWeather.dailyLow)°"
+        }
+    }
+    
+}
